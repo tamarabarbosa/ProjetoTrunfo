544ad3a4aa26b3a0e99189499aab4c97a0628e77
Tamara <tamara-bt@hotmail.com>
2013-11-18 13:19:42 -0200

    Criando classes

---
+++ Trunfo/src/trunfo/Trunfo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trunfo;
+
+/**
+ *
+ * @author tamara
+ */
+public class Trunfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
