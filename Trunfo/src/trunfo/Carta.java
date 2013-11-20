371804a7717dc9717e9fb2a11a9be2d47108e965
Tamara <tamara-bt@hotmail.com>
2013-11-20 15:36:00 -0200

    Gerando e adaptando getters and setters necessários

--- Trunfo/src/trunfo/Carta.java
+++ Trunfo/src/trunfo/Carta.java
@@ -16,5 +16,74 @@ public class Carta {
     private int equipamento;
     private int inteligencia;
     private static Deck deck=null;
+
+    public String getNomeCarta() {
+        return nomeCarta;
+    }
+
+    public void setNomeCarta(String nomeCarta) {
+        this.nomeCarta = nomeCarta;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int forca) {
+        if (forca<=1 || forca>6 )
+            this.forca = 1;
+        else
+            this.forca = forca;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        if (velocidade<=1 || velocidade>6 )
+            this.velocidade = 1;
+        else
+            this.velocidade = velocidade;
+    }
+
+    public int getHabilidade() {
+        return habilidade;
+    }
+
+    public void setHabilidade(int habilidade) {
+        if (habilidade<=1 || habilidade>6 )
+            this.habilidade = 1;
+        else
+            this.habilidade = habilidade;
+    }
+
+    public int getEquipamento() {
+        return equipamento;
+    }
+
+    public void setEquipamento(int equipamento) {
+        if (equipamento<=1 || equipamento>6 )
+            this.equipamento = 1;
+        else
+            this.equipamento = equipamento;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        if (inteligencia<=1 || inteligencia>6 )
+            this.inteligencia = 1;
+        else
+            this.inteligencia = inteligencia;
+    }
+
+    public static Deck getDeck() {
+        return deck;
+    }
+    
+    
     
 }
